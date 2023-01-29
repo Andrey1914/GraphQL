9d4b049da964333461df522574c3ec042f7dc22a
Andrey1914 <anlikajw@gmail.com>
2023-01-29 17:35:23 +0100

    added cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("../schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3005;
@@ -11,6 +12,7 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
