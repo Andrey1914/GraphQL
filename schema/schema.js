04f3614820048c284a1f85db77dd6f27cbdc6254
Andrey1914 <anlikajw@gmail.com>
2023-01-29 17:52:32 +0100

    deleted comments

--- schema/schema.js
+++ schema/schema.js
@@ -14,47 +14,6 @@ const {
 const Movies = require("../models/movie");
 const Directors = require("../models/director");
 
-// const directorsJson = [
-//   { name: "Quentin Tarantino", age: 55 }, {"_id":{"$oid":"63adb1952304a95856958104"}}
-//   { name: "Michael Radford", age: 72 }, {"_id":{"$oid":"63adb5ee2304a95856958109"}}
-//   { name: "Andrei Kurka", age: 40 }, {"_id":{"$oid":"63adb6782304a9585695810b"}}
-//   { name: "Guy Ritchie", age: 50 }, {"_id":{"$oid":"63adb72c2304a9585695810d"}}
-// ];
-
-// const moviesJson = [
-//   { name: "Pulp Fiction", genre: "Crime", directorId: "63adb1952304a95856958104"},
-//   { name: "1984", genre: "Sci-Fi", directorId: "63adb5ee2304a95856958109"},
-//   { name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "63adb6782304a9585695810b"},
-//   { name: "Snatch", genre: "Crime-Comedy", directorId: "63adb72c2304a9585695810d"},
-//   { name: "Reservior Dogs", genre: "Crime", directorId: "63adb1952304a95856958104"},
-//   { name: "The Hateful Eight", genre: "Crime", directorId: "63adb1952304a95856958104"},
-//   { name: "Inglourious Basterds", genre: "Crime", directorId: "63adb6782304a9585695810b"},
-//   { name: "Lock, Stock and Two Smoking Barrels", genre: "Crime-Comedy", directorId: "63adb72c2304a9585695810d"},
-// ];
-
-// const movies = [
-//   { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "1" },
-//   { id: "2", name: "1984", genre: "Sci-Fi", directorId: "2" },
-//   { id: "3", name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
-//   { id: "4", name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
-//   { id: "5", name: "Reservior Dogs", genre: "Crime", directorId: "1" },
-//   { id: "6", name: "The Hateful Eight", genre: "Crime", directorId: "1" },
-//   { id: "7", name: "Inglourious Basterds", genre: "Crime", directorId: "3" },
-//   {
-//     id: "8",
-//     name: "Lock, Stock and Two Smoking Barrels",
-//     genre: "Crime-Comedy",
-//     directorId: "4",
-//   },
-// ];
-
-// const directors = [
-//   { id: "1", name: "Quentin Tarantino", age: 55 },
-//   { id: "2", name: "Michael Radford", age: 72 },
-//   { id: "3", name: "Andrei Kurka", age: 40 },
-//   { id: "4", name: "Guy Ritchie", age: 50 },
-// ];
-
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
@@ -66,7 +25,6 @@ const MovieType = new GraphQLObjectType({
     director: {
       type: DirectorType,
       resolve(parent, args) {
-        // return directors.find((director) => director.id == parent.id);
         return Directors.findById(parent.directorId);
       },
     },
@@ -82,7 +40,6 @@ const DirectorType = new GraphQLObjectType({
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        // return movies.filter((movie) => movie.directorId === parent.id);
         return Movies.find({ directorId: parent.id });
       },
     },
@@ -191,7 +148,6 @@ const Query = new GraphQLObjectType({
       type: MovieType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // return movies.find((movie) => movie.id == args.id);
         return Movies.findById(args.id);
       },
     },
@@ -199,21 +155,18 @@ const Query = new GraphQLObjectType({
       type: DirectorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // return directors.find((director) => director.id == args.id);
         return Directors.findById(args.id);
       },
     },
     movies: {
       type: new GraphQLList(MovieType),
       resolve(parent, args) {
-        // return movies;
         return Movies.find({});
       },
     },
     directors: {
       type: new GraphQLList(DirectorType),
       resolve(parent, args) {
-        // return directors;
         return Directors.find({});
       },
     },
